477d17df02c366e0700f942d5e7fe6c98ef14398
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2024-12-12 08:35:33 -0800

    Code cleanup.

---
+++ FauxCommon/Integrations/BetterChests/BetterChestsIntegration.cs
@@ -0,0 +1,12 @@
+﻿namespace LeFauxMods.Common.Integrations.BetterChests;
+
+/// <summary>Initializes a new instance of the <see cref="BetterChestsIntegration" /> class.</summary>
+/// <param name="modRegistry">Dependency used for fetching metadata about loaded mods.</param>
+internal sealed class BetterChestsIntegration(IModRegistry modRegistry) : ModIntegration<IBetterChestsApi>(modRegistry)
+{
+    /// <inheritdoc />
+    public override string UniqueId => "furyx639.BetterChests";
+
+    /// <inheritdoc />
+    public override ISemanticVersion Version { get; } = new SemanticVersion(1, 0, 0);
+}
---
+++ FauxCommon/Integrations/BetterChests/IBetterChestsApi.cs
@@ -0,0 +1,8 @@
+﻿namespace LeFauxMods.Common.Integrations.BetterChests;
+
+#pragma warning disable
+
+/// <summary>Mod API for Better Chests.</summary>
+public interface IBetterChestsApi
+{
+}
