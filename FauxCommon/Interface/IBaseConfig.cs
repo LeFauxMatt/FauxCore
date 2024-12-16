f6af0fa14ab25f10b80a0c6cb7ac7716df16bc79
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2024-12-16 09:45:11 -0800

    Refactor config management system
    Removed the `IBaseConfig` interface and `BaseConfigManager` class. Introduced a new `ConfigManager` class for managing configuration file saving and loading. Updated project file paths to reflect these changes.

--- FauxCommon/Interface/IBaseConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace LeFauxMods.Common.Interface;
-
-/// <summary>Represents the base configuration.</summary>
-internal interface IBaseConfig
-{
-    /// <summary>Copy config value from this to another.</summary>
-    /// <param name="other">The config instance to copy to.</param>
-    public void CopyTo(IBaseConfig other);
-}
