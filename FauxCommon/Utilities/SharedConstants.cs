7c67ce6939bedce05c7ae860bf0d43ff44124880
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2024-12-31 14:07:04 -0800

    Add SharedConstants and update project files
    - Introduced `SharedConstants.cs` with constant IDs for UI elements.
    - Updated `FauxCommon.projitems` to include the new file in compilation.
    - Modified `GlobalSuppressions.cs` for consistent suppression formatting.

---
+++ FauxCommon/Utilities/SharedConstants.cs
@@ -0,0 +1,10 @@
+namespace LeFauxMods.Common.Utilities;
+
+internal static class SharedConstants
+{
+    public const int UpArrowId = 69_420;
+
+    public const int DownArrowId = 69_421;
+
+    public const int OrganizeButtonId = 69_422;
+}
