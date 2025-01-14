928bfa8b293070fe9e24b9fe3426ff394f7ce680
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2025-01-13 18:18:08 -0800

    Refactor project files and enhance API documentation
    Updated file paths in `FauxCommon.projitems` for consistent casing and restored utility files. Added XML documentation to the `AddToolbarIcon` method in `IIconicFrameworkApi.cs` for improved clarity. Minor readability improvements made in the `GetUniqueId` method of `CommonHelper.cs`.

--- FauxCommon/Integrations/IconicFramework/IIconicFrameworkApi.cs
+++ FauxCommon/Integrations/IconicFramework/IIconicFrameworkApi.cs
@@ -20,6 +20,21 @@ public interface IIconicFrameworkApi : IIconicFrameworkApiObsolete
         Func<string>? getTitle,
         Func<string>? getDescription);
 
+    /// <summary>Adds an icon</summary>
+    /// <param name="id">A unique identifier for the icon.</param>
+    /// <param name="texturePath">The path to the texture icon.</param>
+    /// <param name="sourceRect">The source rectangle of the icon.</param>
+    /// <param name="getTitle">Text to appear as the title in the Radial Menu.</param>
+    /// <param name="getDescription">Text to appear when hovering over the icon.</param>
+    /// <param name="onClick">An action to perform when the icon is pressed.</param>
+    public void AddToolbarIcon(
+        string id,
+        string texturePath,
+        Rectangle? sourceRect,
+        Func<string>? getTitle,
+        Func<string>? getDescription,
+        Action onClick);
+
     /// <summary>Removes an icon.</summary>
     /// <param name="id">A unique identifier for the icon.</param>
     public void RemoveToolbarIcon(string id);
