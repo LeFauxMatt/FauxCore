36273a14485b79841748d24efcea02ab49e1480c
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2025-02-07 17:08:50 -0800

    Refactor integration from RadialMenu to StarControl
    Updated `FauxCommon.projitems` to remove RadialMenu references and add StarControl integration. Removed `IRadialMenuApi.cs` and `RadialMenuIntegration.cs`, and added `IStarControlApi.cs` and `StarControlIntegration.cs`. This change introduces new methods and enums for Star Control functionality, marking a shift in focus within the project.

--- FauxCommon/Integrations/StarControl/StarControlIntegration.cs
+++ FauxCommon/Integrations/StarControl/StarControlIntegration.cs
@@ -1,10 +1,10 @@
-namespace LeFauxMods.Common.Integrations.RadialMenu;
+namespace LeFauxMods.Common.Integrations.StarControl;
 
-/// <summary>Initializes a new instance of the <see cref="RadialMenuIntegration" /> class.</summary>
+/// <summary>Initializes a new instance of the <see cref="StarControlIntegration" /> class.</summary>
 /// <param name="modRegistry">Dependency used for fetching metadata about loaded mods.</param>
 /// <param name="required">Determines if the mod is required.</param>
-internal sealed class RadialMenuIntegration(IModRegistry modRegistry, bool required = false)
-    : ModIntegration<IRadialMenuApi>(modRegistry, required)
+internal sealed class StarControlIntegration(IModRegistry modRegistry, bool required = false)
+    : ModIntegration<IStarControlApi>(modRegistry, required)
 {
     /// <inheritdoc />
     public override string UniqueId => "focustense.RadialMenu";
