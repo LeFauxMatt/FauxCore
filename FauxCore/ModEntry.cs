48024d676c7febd14e34a6527ea0aee65f78e0ef
Matthew <3005344+LeFauxMatt@users.noreply.github.com>
2025-02-13 18:18:38 -0800

    Add button press handling for load game menu
    Introduces `OnButtonPressed` event handler to manage button presses in the title menu. It checks for active menu conditions and updates the last save configuration based on the selected slot button. Modifies `OnSaveLoaded` to properly unsubscribe from the button press event when a save is loaded.

--- FauxCore/ModEntry.cs
+++ FauxCore/ModEntry.cs
@@ -18,18 +18,47 @@ internal sealed class ModEntry : Mod
         // Events
         helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
         helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+        helper.Events.Input.ButtonPressed += OnButtonPressed;
     }
 
     /// <inheritdoc />
     public override object GetApi(IModInfo mod) => new ModApi(mod);
 
+    private static void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+        if (Game1.activeClickableMenu is not TitleMenu ||
+            TitleMenu.subMenu is not LoadGameMenu menu ||
+            ModState.ConfigWithLastSave is null)
+        {
+            return;
+        }
+
+        var (mouseX, mouseY) = e.Cursor.GetScaledScreenPixels().ToPoint();
+        var slotButton = menu.slotButtons.FirstOrDefault(slot => slot.containsPoint(mouseX, mouseY));
+        if (slotButton is null)
+        {
+            return;
+        }
+
+        var index = menu.slotButtons.IndexOf(slotButton);
+        if (index == -1)
+        {
+            return;
+        }
+
+        var menuSlot = menu.MenuSlots[menu.currentItemIndex + index];
+        if (menuSlot is not LoadGameMenu.SaveFileSlot saveFileSlot)
+        {
+            return;
+        }
+
+        ModState.ConfigWithLastSave.LastSave = saveFileSlot.Farmer.slotName;
+    }
+
     private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
     {
         this.Helper.Events.GameLoop.UpdateTicked -= this.OnUpdateTicked;
-        if (ModState.ConfigWithLastSave is not null)
-        {
-            ModState.ConfigWithLastSave.LastSave = Game1.player.slotName;
-        }
+        this.Helper.Events.Input.ButtonPressed -= OnButtonPressed;
     }
 
     private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
